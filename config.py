ed9a2a4a49b76d499f9ae2a63449d5013d5e1b3a
khakimovallamurod <allamurod0206@gmail.com>
2025-07-07 20:35:31 +0500

    add function

--- config.py
+++ config.py
@@ -14,3 +14,13 @@ def get_url():
         raise ValueError("URL not found.")
     return URL
 
+def get_access_token():
+    ACCESS_TOKEN = os.getenv("access_token")
+    if ACCESS_TOKEN is None:
+        raise ValueError("ACCESS_TOKEN not found.")
+    return ACCESS_TOKEN
+def get_xsrf_token():
+    XSRF_TOKEN = os.getenv("xsrf_token")
+    if XSRF_TOKEN is None:
+        raise ValueError("XSRF_TOKEN not found.")
+    return XSRF_TOKEN
